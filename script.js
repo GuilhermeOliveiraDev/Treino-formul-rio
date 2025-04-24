9d48d5632de74a65f2ef109043ae012edc70df79
DGLR <oliveir4.dg@gmail.com>
2025-04-24 05:31:55 -0300

    First commit,page  done.

---
+++ script.js
@@ -0,0 +1,23 @@
+const users = []
+const listElement = document.getElementById("list");
+addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(event);
+    const user = {
+        name:event.target[0].value,
+        email:event.target[1].value,
+        age:event.target[2].value,
+    };
+    users.push(user);
+    updateRegisteredList(listElement)
+    console.log(users);
+});
+
+function updateRegisteredList(list) {
+    list.innerHTML = "<br>";
+    users.length ?
+        users.forEach(user => {list.innerHTML += `<li>${user.name} (${user.email},  ${user.age})</li><br>`})
+        : list.innerHTML += "<p>No users registered</p>";
+};
+console.log(listElement);
+updateRegisteredList(listElement);
